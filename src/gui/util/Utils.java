a4072c15f56821154a17facc45a44458c2044d2a
Leonardo Oliveira <leonardo_sp1997@hotmail.com>
2020-01-27 18:48:34 -0300

    Saving a new Department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -5,7 +5,17 @@ import javafx.scene.Node;
 import javafx.stage.Stage;
 
 public class Utils {
+	
 	public static Stage currentStage(ActionEvent event) {
 		return (Stage)((Node)event.getSource()).getScene().getWindow();
 	}
+	
+	public static Integer tryParseToInt(String args) {
+		try {
+			return Integer.parseInt(args);
+		}catch(NumberFormatException e) {
+			return null;
+		}
+	}
+	
 }
