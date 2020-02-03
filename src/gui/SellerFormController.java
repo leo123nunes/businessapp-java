6557c76417e4ee6825e89f1e8d6ca0a31caf35d5
leo123nunes <ti.userleo@hotmail.com>
2020-02-03 11:31:47 -0200

    Saving Seller

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,9 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -131,10 +133,31 @@ public class SellerFormController implements Initializable {
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
 
 		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
-			exception.addError("name", "Name can't be empty.");
+			exception.addError("name", "Field can't be empty.");
 		}
 
 		obj.setName(txtName.getText());
+		
+		if (txtEmail.getText() == null || txtEmail.getText().trim().equals("")) {
+			exception.addError("email", "Field can't be empty.");
+		}
+		
+		obj.setEmail(txtEmail.getText());
+		
+		if(txtBirthDate.getValue() == null) {
+			exception.addError("birthDate", "Field can't be empty.");
+		}else {
+		Instant instant = Instant.from(txtBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()));
+		obj.setBirthDate(Date.from(instant));
+		}
+		
+		if (baseSalary.getText() == null || baseSalary.getText().trim().equals("")) {
+			exception.addError("baseSalary", "Field can't be empty.");
+		}
+		
+		obj.setBaseSalary(Utils.tryParseToDouble(baseSalary.getText()));
+		
+		obj.setDepartment(comboBoxDepartment.getValue());
 
 		if (exception.getErrors().size() > 0) {
 			throw exception;
@@ -184,10 +207,12 @@ public class SellerFormController implements Initializable {
 
 	private void setErrorMessages(Map<String, String> errors) {
 		Set<String> fields = errors.keySet();
+		
+		labelNameError.setText((fields.contains("name") ? errors.get("name") : ""));
+		labelEmailError.setText((fields.contains("email") ? errors.get("email") : ""));
+		labelBirthDateError.setText((fields.contains("birthDate") ? errors.get("birthDate") : ""));
+		labelBaseSalaryError.setText((fields.contains("baseSalary") ? errors.get("baseSalary") : ""));
 
-		if (fields.contains("name")) {
-			labelNameError.setText(errors.get("name"));
-		}
 	}
 
 	public void loadAssociatedObjects() {
--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -27,6 +27,14 @@ public class Utils {
 			return null;
 		}
 	}
+	
+	public static Double tryParseToDouble(String args) {
+		try {
+			return Double.parseDouble(args);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
 
 	public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) {
 		tableColumn.setCellFactory(column -> {
